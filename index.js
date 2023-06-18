4ca0157b68fc622f710fc36cf092c766f7378bbb
gift kimani <gift.kimani@students.moringaschool.com>
2023-06-18 22:18:23 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+    const messages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      messages.push(message);
+    }
+  
+    return messages;
+  }
+  
+  const names = ['Alice', 'Bob', 'Carol'];
+  const eventName = 'birthday';
+  
+  const cards = writeCards(names, eventName);
+  console.log(cards);
+  
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  countDown(11);
